e87cca716c25b6be2fee3cd87c77e3c5dac7aa52
Bobby-la-brioche <noe.thivolle@gmail.com>
2023-12-09 19:07:12 +0100

    le trait qui suit la souris

--- le_jeu_de_la_pasteque.py
+++ le_jeu_de_la_pasteque.py
@@ -8,8 +8,8 @@ APP_LOGO = "res/logo.png"
 
 WINDOW_SIZE = (1236, 651)
 WINDOW_BACKGROUND = "res/fond_boite_2.png"
-SOURIS= "res/carrotte2.png"
-
+SOURIS= "res/carotte2.png"
+NUAGE= "res/nuage.png"
 
 pygame.init()
 pygame.display.set_caption(APP_TITLE)
@@ -34,53 +34,79 @@ background2 = background
 
 #Souris
 
-new_cursor = pygame.image.load(SOURIS)
-mouse =    pygame.mouse.get_pos()
+curseur_carotte     = pygame.image.load(SOURIS).convert_alpha()
+curseur_nuage       = pygame.image.load(NUAGE).convert_alpha()
+mouse               = pygame.mouse.get_pos()
+curseur_carotte2    = curseur_carotte
+curseur_nuage2      = curseur_nuage
+
+taille_curseur_carotte=curseur_carotte.get_size()
+taille_curseur_nuage=curseur_nuage.get_size()
+
 
 #Couleurs
 
-YELLOW =   (255, 255, 0)
-RED =      (255, 0, 0)
-GREEN =    (0, 255, 0)
-BLANC =    (255,255,255)
-NOIR =     (0, 0, 0)
+YELLOW  = (255, 255, 0)
+GREEN   = (0, 255, 0)
+BLANC   = (255,255,255)
+ROUGE   = (255, 0, 0)
+NOIR    = (0, 0, 0)
+GRIS    = (155,155,155)
+ROSE    = (180,50, 67)
 
 fruits = []
 
 
+#music
 file = 'res/music_pasteque.mp3'
 pygame.mixer.init()
 pygame.mixer.music.load(file)
 pygame.mixer.music.play(-1)
 
 
+#fonctions
+
+def ratio(image):
+    a=image.get_size()
+    return(a[0]/a[1])
+
+def taille(image):
+    a=image.get_size()
+    return(a[0],a[1])
+
 while True:
     # actions utilisateurs (clavier, souris)
     # Les évènements regardés 
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         
         elif event.type == pygame.VIDEORESIZE:
-            background2 = pygame.transform.scale(background,pygame.display.get_surface().get_size())
+            WINDOW_SIZE=pygame.display.get_surface().get_size()
+            background2 = pygame.transform.scale(background,WINDOW_SIZE)
+            curseur_carotte2 = pygame.transform.scale(curseur_carotte,(taille(curseur_carotte)[0]*WINDOW_SIZE[0]/1000*ratio(curseur_carotte),taille(curseur_carotte)[1]*WINDOW_SIZE[1]/1000))
+            
+
+        # mise à jour des données 
+    # pas encore fait 
  
-    if mouse[0]<=(2/3 *WINDOW_SIZE[0]) and mouse[0]>=(1/3 *WINDOW_SIZE[0]):
-        pygame.draw.line(NOIR, start_pos=mouse, end_pos=(mouse[0],WINDOW_SIZE[1]))
-    
+    # affichage 
+    ecran.fill(NOIR) 
+    ecran.blit(background2, (0, 0)) 
+     
+    mouse = pygame.mouse.get_pos()
 
 
 
+    if mouse[0]>=(7/24 *WINDOW_SIZE[0]) and mouse[0]<=(149/200 *WINDOW_SIZE[0]):
+        pos=(mouse[0],(101/550)*WINDOW_SIZE[1])
+        pygame.draw.line(ecran, ROSE, start_pos=pos, end_pos=(mouse[0],WINDOW_SIZE[1]), width=5)
+        ecran.blit(curseur_nuage2,pos)
+    else:
+        ecran.blit(curseur_carotte2, mouse)
 
-    # mise à jour des données
-    # pas encore fait
-
-    # affichage
-    ecran.fill(NOIR)
-    ecran.blit(background2, (0, 0))
-    
-    pos = pygame.mouse.get_pos()
-    ecran.blit(new_cursor, pos) # t'as oublié game
 
     clock.tick(120)
     pygame.display.update()
